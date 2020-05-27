c9b29914343cc853dce84b6598e786c516ee2365
hrishiraj22 <64540861+hrishiraj22@users.noreply.github.com>
2020-05-28 02:21:42 +0530

    first

---
+++ mail.py
@@ -0,0 +1,17 @@
+import smtplib
+s = smtplib.SMTP('smtp.gmail.com', 587)
+s.starttls()
+
+s.login("hrishiraj48@gmail.com", "**********")
+
+
+    # message
+message1 = "accuracy is less than 90%.Try again"
+    
+
+    # sending the mail 
+s.sendmail("hrishiraj48@gmail.com", "1706505@kiit.ac.in", message1)
+    
+
+    # terminating the session 
+s.quit()
